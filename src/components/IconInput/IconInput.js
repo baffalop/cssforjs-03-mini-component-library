700fbeb9fb2c89ec2aebcc6e9e76d93711c85868
Nikita Gaidakov <ngaidakov@podfather.com>
2025-01-04 21:47:47 +0000

    IconInput: most of small design

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,49 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return <Wrapper w={width}>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <Input placeholder={placeholder} />
+    <InputIcon id={icon} size={16} />
+  </Wrapper>;
 };
+
+const Wrapper = styled.label`
+  position: relative;
+  width: ${p => p.w}px;
+`
+
+const Input = styled.input`
+  appearance: none;
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 4px 8px;
+  padding-left: 24px;
+  
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  
+  &:focus {
+    outline-offset: 2px;
+  }
+  
+  &:hover {
+    color: black;
+  }
+  
+  ::-webkit-input-placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`
+
+const InputIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+`
 
 export default IconInput;
